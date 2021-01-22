413e265c688e395c6a4879595086c9b1ed066ee4
Haakon <aareskjold.haakon@gmail.com>
2021-01-22 21:20:44 +0100

    added logic

--- task3.cpp
+++ task3.cpp
@@ -1,12 +1,21 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include <utility>
+#include <stack>
 
 // Override base class with your custom functionality
 class Example : public olc::PixelGameEngine
 {
 public:
 
-    int* cell;
+    enum flags {
+        SOUTH_FACE = 0x2,
+        EAST_FACE = 0X4,
+        VISITED = 0X8
+    };
+
+    int* cells;
+    std::stack<std::pair<int, int >> stack;
     int height;
     int width;
 
@@ -21,19 +30,47 @@ public:
 	{
         height = 64;
         width = 64;
-		cell = new int[height*width];
+		cells = new int[height*width];
+
+        memset(cells, 0, (height*width));
+
+        stack.push(std::make_pair(0,0));
+        cells[0] |= VISITED;
+        cells[0] |= SOUTH_FACE;
+        cells[1] |= VISITED;
+        cells[0] |= EAST_FACE;
+        cells[width] = VISITED;
     
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+        auto getOffset = [&](int x, int y) {
+            return cells[x + y * width];
+        };
+
 		Clear(olc::Pixel(olc::BLACK));
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                FillRect(x*8, y*8 + 7, 6, 1);
-                FillRect(x*8 + 7, y*8 + 1, 1, 6);
+
+                if (getOffset(x,y) & SOUTH_FACE) {
+                    //South (horizontal lines)
+                    FillRect(x*8, y*8 + 7, 7, 1);
+                }
+                
+            
+                if (getOffset(x,y) & EAST_FACE) {    
+                //East (vertical lines)
+                FillRect(x*8 + 7, y*8, 1, 7);
+                }
+
+                if (getOffset(x,y) & VISITED) {
+                //Fill
+                FillRect(x*8, y*8, 7, 7);
+                }
             }
         }
         return true;
