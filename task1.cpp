821b00b2736afc53a5ca811ee93c08f800496252
Haakon <aareskjold.haakon@gmail.com>
2021-01-22 18:07:31 +0100

    first commit

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Point {
+    int x;
+    int y;
+
+     Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+int main (int argv, char** argc) {
+    for (int i= 0; i < argv; i++) {
+    std::cout << argc[i] << '\n';
+    }
+}
